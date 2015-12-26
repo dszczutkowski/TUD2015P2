11520528a006f53865aee61fddc8860bc86fc445
dszczutkowski <dszczutkowski@outlook.com>
2015-12-26 17:40:08 +0100

    start

---
+++ src/main/java/domain/connectionTest.java
@@ -0,0 +1,17 @@
+package domain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class connectionTest {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("projekt");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
+}
